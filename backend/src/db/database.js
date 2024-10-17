73e80202ebbbb86860e5d9575ba0027af5c01ec0
Leonardo <leoonardoogzz@gmail.com>
2024-10-17 10:13:03 -0600

    Ultimo commit

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -1,9 +1,1 @@
-export const products = [
-  {
-    id: 1,
-    name: "Nose",
-    description: "NOSE",
-    price: 1234,
-    imageURL: "/uploads/1729096978306-416821715.png",
-  },
-];
+export const products = [{}];
